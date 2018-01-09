ace945fcb9caa93490df87a2babf7dedb7313508
enixlin <enixlin@126.com>
2018-01-09 12:45:27 +0800

    snake game move function

--- js/js.js
+++ js/js.js
@@ -1,32 +1,53 @@
 //alert("js.js file is load");
-var n=10;
-var m=10;
+var n = 10;
+var m = 10;
 function doKey(e) {
 	keynum = window.event ? e.keyCode : e.which;
-	console.log("press key is :" + " key code is :" + keynum);
-	
-	var div=document.getElementById("head");
-	
-console.log(div);
+	console.log(" key code is :" + keynum);
+
+	var div = document.getElementById("head");
+
+	//console.log(div);
 	/*
 	 * left arrow 37 up arrow 38 right arrow 39 down arrow 40
 	 * 
 	 */
-	
+
 	if (keynum == 39) {
+
 		n++;
-		div.style.left=n*10+"px";
+		n = checkborder(n);
+		div.style.left = n + "px";
 	}
 	if (keynum == 37) {
+
 		n--;
-		div.style.left=n*10+"px";
+		n = checkborder(n);
+		div.style.left = n+ "px";
 	}
 	if (keynum == 40) {
+
 		m++;
-		div.style.top=m*10+"px";
+		m = checkborder(m);
+		div.style.top = m  + "px";
 	}
 	if (keynum == 38) {
+
 		m--;
-		div.style.top=m*10+"px";
+		m = checkborder(m);
+		div.style.top = m  + "px";
 	}
+
+	console.log("n is :" + n);
+	console.log("m is :" + m);
+}
+
+function checkborder(n) {
+	if (n < 0) {
+		return 0;
+	}
+	if (n > 390) {
+		return 390;
+	}
+	return n;
 }
